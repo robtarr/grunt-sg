bc16ba512e2f8b4712af29b49f6d39e37b4874e5
Rob Tarr <tarr@heysparkbox.com>
2013-10-14 15:35:30 -0400

    feat: Updating to add options for new styleguide module

--- tasks/sg.js
+++ tasks/sg.js
@@ -17,14 +17,16 @@ module.exports = function(grunt) {
     options = {
       css: path.resolve(this.options().css),
       html: path.resolve(this.options().html),
-      dest: path.resolve(this.options().dest)
+      dest: path.resolve(this.options().dest),
+      js: this.options().js || "";
+      logo: this.options().logo || "";
+      syntax: this.options().syntax || "github";
     };
 
-    if(!this.options().data) {
-      options.data = false;
-    }
-    else {
+    if(this.options().data) {
       options.data = path.resolve(this.options().data);
+    } else {
+      options.data = false;
     }
 
     sg = new Styleguide(options);
